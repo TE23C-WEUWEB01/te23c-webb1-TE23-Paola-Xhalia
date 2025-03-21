f39b886236240042b57de75aec058e953ad7bb15
Paola Xhalia <paola.xhalia@elev.ga.ntig.se>
2025-03-21 09:44:17 +0100

    Tränat på if-satser och loopar

---
+++ kapitel-8/script.js
@@ -0,0 +1,28 @@
+// while (true) {
+//     var namn = prompt("Ange ditt namn:");
+//     var lösendord = prompt("Ange ditt lösenord:");
+
+//     if (lösenord == "1234") {
+//         document.writeln(`DU ${namn} ÄR INLOGGAD`);
+//         break;
+//     } else {
+//         document.writeln(`DU ${namn} HAR SKRIVIT FEL LÖSENORD`);
+
+//     }
+// }
+
+
+// //Skriv ut talen 20-30
+// for (let index = 20; index < 31; index++) {
+//     document.writeln(`tal: ${index} <br>`);
+    
+// }
+
+//let val = confirm("Klicka OK för att förtsätta!"); // true eller false
+let val= true;
+while (val) {
+    val= confirm ("Klicka OK för att fortsätta!"); //true eller false
+
+
+    
+}
